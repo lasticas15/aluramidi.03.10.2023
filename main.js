839b12fbee63393e7e07b364b46826685c792103
LaStIcA1515 <104463065+lasticas15@users.noreply.github.com>
2023-10-03 14:50:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// Pom ---------------------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+ //document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+ document.querySelectorAll('.tecla')
+ 
+
+
+
+
+
+
+
